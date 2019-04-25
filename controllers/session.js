e37e5969da5089a1efefd0728a7bff9864c41b12
Javier Delgado <javier_180796@hotmail.com>
2019-04-24 20:18:09 -0400

    Nuevo register

--- controllers/session.js
+++ controllers/session.js
@@ -4,11 +4,33 @@ const auth = require('./../middlewares/isAuth')
 let router = express.Router();
 
 router.post('/createUser',(req,res)=>{
+  var newUser = {
+    username: "Zirulxions",
+    password: "123456789",
+    name: "Javier Delgado",
+    email: "javierzirulxions@gmail.com"
+  }
   db.connect().then((obj)=>{
-    obj.one('INSERT INTO users (user_password, user_username, user_nname, user_creation_time, user_email) VALUES ();')
-  })
+    obj.one("INSERT INTO users (user_password, user_username, user_name, user_creation_time, user_email) VALUES ('"+newUser.password+"','"+newUser.username+"','"+newUser.name+"',CURRENT_TIMESTAMP,'"newUser.email"')")
+  }).catch((error)=>{
+    console.log(error);
+    res.send({
+      error:error,
+      msg:'No se ha creado el usuario',
+      status:500
+    });
+    obj.done();
+  }).catch((error)=>{
+    console.log(error);
+    res.send({
+      error:error,
+      msg:'No se ha creado el usuario',
+      status:500
+    });
+  });
 });
 
+module.exports = router;
 /*
 router.get('/value', auth.isAuth, (req, res) => {
     res.send(req.session.passport);
@@ -48,5 +70,3 @@ router.get('/logout', auth.isAuth, function (req, res) {
     });
 });
 */
-
-module.exports = router;
