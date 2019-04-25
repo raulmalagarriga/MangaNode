9dd35db6e6877e5659648ca2d2534376cb7ace46
Javier Delgado <javier_180796@hotmail.com>
2019-04-25 01:42:33 -0400

    Login Logout
    FAINALII

--- controllers/session.js
+++ controllers/session.js
@@ -8,7 +8,7 @@ const bcrypt = require('bcryptjs');
 route.post('/createUser',(req,res)=>{
   var newUser = {
     username: req.body.username,
-    password: bcrypt.hashSync(req.body.password, 10), //bcrypt.hashSync('myPassword', null);
+    password: bcrypt.hashSync(req.body.password, 10),
     name: req.body.name,
     email: req.body.email
   };
@@ -43,77 +43,30 @@ route.post('/login', auth.isLogged, function (req, res, next) {
       return next(err);
     }
     if (!user) {
-      return res.status(401).json({
+      return res.status(401).send({
         err: info
       });
     }
     req.logIn(user, function(err){
       if (err) {
-        return res.status(500).json({
+        return res.status(500).send({
           err: 'No ha sido posible iniciar sesion'
         });
       }
-      res.status(200).json({
+      res.status(200).send({
         status: 'Sesion iniciaida'
       });
     });
   })(req, res, next);
-    console.log(req.session);
-  });
-/*
-  var logUser = {
-    username: req.body.username,
-    password: req.body.password
-  };
-  db.connect().then((obj)=>{
-    obj.one("SELECT * FROM users WHERE user_username='"+logUser.username+"' AND user_password='"+logUser.password+"'")
-    .then((data)=>{
-      console.log(data);
-      res.send({data:data,status:200});
-      obj.done();
-    }).catch((error)=>{
-      console.log(error);
-      res.send({error:error,msg:'Usuario no conseguido',status:404});
-      obj.done();
-    });
-  }).catch((error)=>{
-    console.log(error);
-    res.send({error:error,msg:'Algo salio mal',status:500});
-  });
-  console.log("Logeado "+logUser);
-  passport.authenticate('local',function(req, username, password, done){
+  console.log("Session ID: "+req.session.id);
+  console.log(req.session)
+});
 
-  });
-
-  */
-  /*
-   passport.authenticate('local', function (err, user, info) {
-        if (err) {
-            return next(err);
-        }
-        if (!user) {
-            return res.status(401).send({
-                err: info
-            });
-        }
-        req.logIn(user, function (err) {
-            if (err) {
-                return res.status(500).send({
-                    err: 'Could not log in user'
-                });
-            }
-            res.status(200).send({
-                status: 'Login successful!'
-            });
-        });
-    })(req, res, next);*/
-//    console.log(req.session);
-//});
-
-route.get('/logout', auth.isAuth, function (req, res) {
+route.get('/logout', function (req, res) {
     req.logout();
+    console.log("Hasta la vista BEIBI");
     res.status(200).send({
-        status: 'Bye!'
+        status: 'Adios..!'
     });
 });
 
